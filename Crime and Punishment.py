96952452041ec11a9a76a6568ae37418bafeddcf
Yusuf Oztoprak <147493350+YusufOztoprak@users.noreply.github.com>
2024-02-28 16:42:56 +0300

    Add files via upload

---
+++ Crime and Punishment.py
@@ -0,0 +1,26 @@
+
+
+class Book:
+    def __init__(self, title, author, pages):
+        print("A book is created")
+        self.title = title
+        self.author = author
+        self.pages = pages
+    
+    def __str__(self):
+        return "Title: {0} \nAuthor :{1} \nPages: {2}".format(self.title, self.author, self.pages)
+
+    def __len__(self):
+        return self.pages
+
+    def __del__(self):
+        print("A book is destroyed")
+        
+
+print(Book("Crime and Punishment", "Fyodor Dostoevsky", 560))
+
+
+
+
+
+
